647b8cbefb5e93a769b971fcbefbc6e574b03cf0
Colm <colm.m.cavanagh@gmail.com>
2018-01-17 21:23:13 +0000

    fixing python code

--- ClassifyNB.py
+++ ClassifyNB.py
@@ -1,14 +1,12 @@
 
-def classify(features_train, labels_train): 
-  from sklearn.naive_bayes import GaussianNB
+def classify(features_train, labels_train):
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    pred = clf.pred()
     ### import the sklearn module for GaussianNB
     ### create classifier
     ### fit the classifier on the training features and labels
     ### return the fit classifier
- 
-    clf = GaussianNB()
-	clf.fit(features_train,labels_train)
-	pred = clf.pred()
+
     ### your code goes here!
-    
-    
